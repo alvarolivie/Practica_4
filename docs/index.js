379d2db0905dff3de69939fdf9c782ebbbb37bce
alvarolivie <alvarolivie@gmail.com>
2022-02-25 16:45:35 +0100

    funciona

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,5 @@
 async function getRates() {
     const curr = ["USD","EUR","AUD","GBP","CAD"];
-    let price = new Array(4);
     let url = "https://v6.exchangerate-api.com/v6/2d734ce2d1971062c4c3f91d/latest/";
     let currency = document.getElementById("currency").value;
     console.log(currency);
@@ -12,15 +11,13 @@ async function getRates() {
     if (response.ok){
     let res = await response.json();
     let rates = res.conversion_rates;
-    for (let i = 0; i < price.length; i++) {
+    for (let i = 0; i < curr.length; i++) {
       if (curr[i]!=currency){
-        price[i] = rates[curr[i]];
-        tdata += '<tr><td>'+curr[i]+'</td><td>'+price[i]+'</td></tr><br>'
+        tdata += '<tr><td>'+curr[i]+'</td><td>'+rates[curr[i]]+'</td></tr><br>'
       }
     }
     let start = '<tr><th>Exchange Rates</th></tr><br>';
     document.getElementById("table").innerHTML = start + tdata;
-    console.log(price);
     }
     return 0;
 
